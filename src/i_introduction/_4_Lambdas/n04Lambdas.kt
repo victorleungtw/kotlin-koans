e64690ea83a36d14ba359098b63b72691dd3b593
Victor Leung <victor.tw.leung@hk.ey.com>
2018-05-24 14:48:06 +0800

    Implement test 4

--- src/i_introduction/_4_Lambdas/n04Lambdas.kt
+++ src/i_introduction/_4_Lambdas/n04Lambdas.kt
@@ -1,5 +1,7 @@
 package i_introduction._4_Lambdas
 
+import com.google.common.base.Predicate
+import com.google.common.collect.Iterables
 import util.TODO
 import util.doc4
 
@@ -11,15 +13,23 @@ fun example() {
     sum(1, square(2)) == 5
 }
 
-fun todoTask4(collection: Collection<Int>): Nothing = TODO(
-    """
-        Task 4.
-        Rewrite 'JavaCode4.task4()' in Kotlin using lambdas:
-        return true if the collection contains an even number.
-        You can find the appropriate function to call on 'Collection' by using code completion.
-        Don't use the class 'Iterables'.
-    """,
-    documentation = doc4(),
-    references = { JavaCode4().task4(collection) })
+//fun todoTask4(collection: Collection<Int>): Nothing = TODO(
+//    """
+//        Task 4.
+//        Rewrite 'JavaCode4.task4()' in Kotlin using lambdas:
+//        return true if the collection contains an even number.
+//        You can find the appropriate function to call on 'Collection' by using code completion.
+//        Don't use the class 'Iterables'.
+//    """,
+//    documentation = doc4(),
+//    references = { JavaCode4().task4(collection) })
+
+
+fun todoTask4(collection: Collection<Int>): Boolean {
+
+    return collection.any {
+        it % 2 == 0
+    }
+}
 
 fun task4(collection: Collection<Int>): Boolean = todoTask4(collection)
